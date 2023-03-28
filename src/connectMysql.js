1bb044fa76e09ce11701675c3d70903cabc295fe
Mrleon123 <Mrleon@hotmail.it>
2023-03-28 13:06:12 +0200

    feat()

--- src/connectMysql.js
+++ src/connectMysql.js
@@ -6,11 +6,7 @@ async function db() {
     host: process.env.LOCALHOST,
     user: process.env.ROOT,
     password: process.env.PASSWORD,
-<<<<<<< HEAD
     port: process.env.PORT_DB
-=======
-    port: process.env.PORT_DB,
->>>>>>> 0862bdfb77f7b22202f4dcbbdd208269fd9f32e1
   });
 }
 
