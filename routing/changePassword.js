97d3d76cb233878dd0b495c2e0e083e97331f312
Mrleon123 <Mrleon@hotmail.it>
2023-04-28 12:08:35 +0200

    feat(changePassword)

--- routing/changePassword.js
+++ routing/changePassword.js
@@ -8,14 +8,31 @@ const router = express.Router();
 
 router.put("/", async (req, res) => {
   const { password, newPassword } = req.body;
+  let hashedPassowrd = await bcrypt.hash(newPassword, 12);
+
+  connection()
+    .then(async (connection) => {
+      await connection.query(queries.use);
+      connection
+        .query(queries.updatePassword, [hashedPassowrd, req.username])
+        .then((rows) => {
+          if (rows.affectedRows === 1) res.json({ status: 201 });
+          else res.json({ status: 201 });
+        });
+    })
+    .catch((error) => {
+      throw error;
+    });
+
     let hashedPassowrd = await bcrypt.hash(newPassword, 12);
     connection()
       .then(async (connection) => {
         await connection.query(queries.use);
         connection
           .query(queries.updatePassword, [hashedPassowrd, req.username])
-          .then(() => {
-            res.json({ status: 201});
+          .then((rows) => {
+            if (rows.length > 0) res.json({ status: 201}).end()
+            else res.json({ status: 400 }).end()
           });
       })
       .catch((error) => {
